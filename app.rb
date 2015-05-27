57378a5f059a1c28f94e33cae9c73340dfdb925a
Dan <agnard.daniel@laposte.net>
2015-05-27 09:22:49 +0200

    restructuration de l'arborescence

---
+++ app.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+
+ActiveRecord::Base.establish_connection(
+	::adapter => 'sqlite3'
+	::database => 'dev.db'
+	)
+
+ActiveRecord::Base.default_timezone=:local
+
