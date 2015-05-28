b7334fe16ca0ee0e53c98a4e15fdec041760330b
Dan <agnard.daniel@laposte.net>
2015-05-28 14:58:05 +0200

    correctif chemin + app.rb

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+
+ActiveRecord::Base.establish_connection(
+	:adapter => 'sqlite3',
+	:database => 'dev.db'
+	)
+
+ActiveRecord::Base.default_timezone=:local
+
+class Produit < ActiveRecord::Base
+end
+
+get '/' do 
+	@tableproduit = Produit.all
+	erb :accueil
+end
