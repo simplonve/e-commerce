4345e1794c19f9a34c801914fd7c9b2517380dbd
Xavier07 <albator98@hotmail.com>
2015-06-01 16:29:45 +0200

    fichier app.rb ajout post /ajout, ajout dans dev.db table des colonnes produit_id et validation et modification page fiche produit

--- app.rb
+++ app.rb
@@ -9,6 +9,11 @@ ActiveRecord::Base.establish_connection(
 ActiveRecord::Base.default_timezone=:local
 
 class Produit < ActiveRecord::Base
+  has_many :commandes
+end
+ 
+class Commande < ActiveRecord::Base
+  belongs_to :produit
 end
 
 get '/' do 
@@ -17,10 +22,17 @@ get '/' do
 end
 
 get '/panier' do
+	@commande = Commande.all
 	erb :panier
 end
 
 get '/:id' do
 	@produit_select = Produit.find(params[:id])
 	erb :fiche_produit
+end
+
+post '/ajout' do
+	@ajout_produit_panier = Commande.new(params[:test])
+	@ajout_produit_panier.save
+	redirect "/"
 end
